0d89af4341430b56d497c9e38ee36dac9b9d465d
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-04-04 20:55:26 +0545

    first commit

---
+++ day 2/sum.py
@@ -0,0 +1,5 @@
+# WAP to add two numbers entered by the user and print the result.
+a= int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+sum = a+b
+print(sum) # This will print the sum of the two numbers entered by the user.
