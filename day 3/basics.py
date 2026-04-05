8bfbe634028da49a7b2f61523abbc25326738b0b
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-04-05 17:32:33 +0545

    Syntax, Comments and Statement in python

---
+++ day 3/basics.py
@@ -0,0 +1,29 @@
+# Syntax, Comments and Statement in python
+
+# Syntax is the set of rules that defines how a Python program will be written and interpreted. It includes things like how to write variables, functions, loops, and other constructs. Example of syntax in Python are:
+# 1. Variable assignment: x = 5
+# 2. Function definition: def my_function():
+# 3. If statement: if x > 0:
+# 4. For loop: for i in range(5):
+# 5. Print statement: print("Hello, World!")
+
+# This is a comment in Python - Comments are used to explain the code and make it more readable. They are ignored by the Python interpreter and do not affect the execution of the program. In Python, comments can be created using the # symbol for single-line comments or triple quotes (''' or """) for multi-line comments.
+
+# multi-line comment in Python
+
+"""
+This is a multi-line comment in Python
+It can span multiple lines and is often used to provide detailed explanations or documentation for code.
+"""
+
+#statement in python is a line of code that performs a specific action. It can be a simple assignment statement, a function call, or a control flow statement like an if statement or a loop. Example of statements in Python are:
+# print("Hello, World!") - This is a statement that prints "Hello, World!" to the console.
+# x = 5 - This is a statement that assigns the value 5 to the variable x.
+
+
+# WAP to add two numbers and print the result
+
+a= 4
+b=5
+c= a+b
+print(c)
---
+++ day 3/name.py
@@ -0,0 +1,12 @@
+# Printing name and profession
+print("Bishal Rijal")
+print("I am a student of MCA")
+print("I am teaching Python programming")
+
+
+name = "Bishal Rijal is a teacher."
+print(name)
+
+a=6
+b=a
+print(b)
