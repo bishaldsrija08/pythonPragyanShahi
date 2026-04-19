0c066fd13ace4e7f76510aed3812b9f4f644c6cf
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-04-19 12:00:49 +0545

    cw

---
+++ projects/cw.py
@@ -0,0 +1,10 @@
+# WAP to calcualte araa of rectangle, circle, square, trianlge
+"""
+Hint: 
+1. Area of rectangle = length * breadth
+2. Area of circle = 3.14 * radius * radius
+3. Area of square = side * side
+4. Area of triangle = 0.5 * base * height
+"""
+
+# Your job is to calculate the area of the shape based on user input. You can use if-else statements to determine which shape the user wants to calculate the area for, and then prompt them for the necessary dimensions. Finally, you can print out the calculated area to the user.
